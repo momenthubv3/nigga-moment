fba36c5bd6fac88c6f10c989dec40a61489bf26a
momenthubv3 <93601997+momenthubv3@users.noreply.github.com>
2021-11-02 20:14:02 +0500

    Add files via upload

---
+++ shapater nigga hubv1.4 working.lua
@@ -0,0 +1,116 @@
+--made by shapater nigga HOYA
+local ScreenGui = Instance.new("ScreenGui")
+local main = Instance.new("Frame")
+local niggahood = Instance.new("TextButton")
+local TextLabel = Instance.new("TextLabel")
+local breaknigga = Instance.new("TextButton")
+local Arsenal = Instance.new("TextButton")
+local main2 = Instance.new("Frame")
+local madcity = Instance.new("TextButton")
+local shindolife = Instance.new("TextButton")
+--Properties:
+ScreenGui.Parent = game.CoreGui
+
+main.Name = "main"
+main.Parent = ScreenGui
+main.Active = true
+main.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+main.Position = UDim2.new(0.144201979, 0, 0.579527617, 0)
+main.Size = UDim2.new(0, 417, 0, 210)
+main.Active = true
+main.Draggable = true
+
+niggahood.Name = "nigga hood"
+niggahood.Parent = main
+niggahood.BackgroundColor3 = Color3.fromRGB(0, 0, 255)
+niggahood.Position = UDim2.new(-0.000451030763, 0, 0.842488468, 0)
+niggahood.Size = UDim2.new(0, 416, 0, 40)
+niggahood.Font = Enum.Font.SourceSans
+niggahood.Text = "nigga hood"
+niggahood.TextColor3 = Color3.fromRGB(0, 0, 0)
+niggahood.TextScaled = true
+niggahood.TextSize = 14.000
+niggahood.TextWrapped = true
+niggahood.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Biteyz/Smoke/main/source')))()
+end)
+
+TextLabel.Parent = main
+TextLabel.Active = true
+TextLabel.BackgroundColor3 = Color3.fromRGB(85, 85, 255)
+TextLabel.Position = UDim2.new(-0.00045103021, 0, -0.000938737765, 0)
+TextLabel.Size = UDim2.new(0, 415, 0, 89)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "nigga moment"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
+
+breaknigga.Name = "breaknigga"
+breaknigga.Parent = main
+breaknigga.BackgroundColor3 = Color3.fromRGB(172, 0, 0)
+breaknigga.Position = UDim2.new(0.00479616318, 0, 0.652012289, 0)
+breaknigga.Size = UDim2.new(0, 416, 0, 40)
+breaknigga.Font = Enum.Font.SourceSans
+breaknigga.Text = "breaknigga"
+breaknigga.TextColor3 = Color3.fromRGB(0, 0, 0)
+breaknigga.TextScaled = true
+breaknigga.TextSize = 14.000
+breaknigga.TextWrapped = true
+breaknigga.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/chriszrk/Bowser-Hub/main/BowserHubCool", true))()
+end)
+
+Arsenal.Name = "Arsenal"
+Arsenal.Parent = main
+Arsenal.BackgroundColor3 = Color3.fromRGB(255, 178, 23)
+Arsenal.Position = UDim2.new(0.00479616318, 0, 0.41867879, 0)
+Arsenal.Size = UDim2.new(0, 415, 0, 50)
+Arsenal.Font = Enum.Font.SourceSans
+Arsenal.Text = "Arsenal"
+Arsenal.TextColor3 = Color3.fromRGB(0, 0, 0)
+Arsenal.TextScaled = true
+Arsenal.TextSize = 14.000
+Arsenal.TextWrapped = true
+Arsenal.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub'))()
+end)
+
+main2.Name = "main2"
+main2.Parent = ScreenGui
+main2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+main2.Position = UDim2.new(0.142857149, 0, 0.417322814, 0)
+main2.Size = UDim2.new(0, 418, 0, 135)
+main2.Active = true
+main2.Draggable = true
+
+madcity.Name = "madcity"
+madcity.Parent = main2
+madcity.BackgroundColor3 = Color3.fromRGB(170, 255, 255)
+madcity.Position = UDim2.new(0.00717133842, 0, 0.486480057, 0)
+madcity.Size = UDim2.new(0, 416, 0, 47)
+madcity.Font = Enum.Font.SourceSans
+madcity.Text = "madcity"
+madcity.TextColor3 = Color3.fromRGB(0, 0, 0)
+madcity.TextScaled = true
+madcity.TextSize = 14.000
+madcity.TextWrapped = true
+madcity.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptRUs/Banana/main/MadCityFast.lua", true))()
+end)
+
+shindolife.Name = "shindo life"
+shindolife.Parent = main2
+shindolife.BackgroundColor3 = Color3.fromRGB(255, 112, 17)
+shindolife.Position = UDim2.new(0.00718849897, 0, -0.00250639394, 0)
+shindolife.Size = UDim2.new(0, 413, 0, 60)
+shindolife.Font = Enum.Font.SourceSans
+shindolife.Text = "shindo life nigga hub spec"
+shindolife.TextColor3 = Color3.fromRGB(0, 0, 0)
+shindolife.TextScaled = true
+shindolife.TextSize = 14.000
+shindolife.TextWrapped = true
+shindolife.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub'))()
+end)
