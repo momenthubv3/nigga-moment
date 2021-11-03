badd39da0b1f4b9df533c5128fc83994b4a290a5
momenthubv3 <93601997+momenthubv3@users.noreply.github.com>
2021-11-03 21:59:45 +0500

    Update shapater nigga hubv1.4 working.lua

--- shapater nigga hubv1.4 working.lua
+++ shapater nigga hubv1.4 working.lua
@@ -1,115 +1,118 @@
---made by nigga YAYEET
+--made my YAYEET pika hub
 local ScreenGui = Instance.new("ScreenGui")
-local main = Instance.new("Frame")
-local niggahood = Instance.new("TextButton")
+local Frame = Instance.new("Frame")
 local TextLabel = Instance.new("TextLabel")
-local breaknigga = Instance.new("TextButton")
-local Arsenal = Instance.new("TextButton")
-local main2 = Instance.new("Frame")
-local madcity = Instance.new("TextButton")
+local UICorner = Instance.new("UICorner")
+local UIGradient = Instance.new("UIGradient")
+local UIGradient_2 = Instance.new("UIGradient")
+local UICorner_2 = Instance.new("UICorner")
+local close = Instance.new("TextButton")
+local UIGradient_3 = Instance.new("UIGradient")
+local TextLabel_2 = Instance.new("TextLabel")
+local UICorner_3 = Instance.new("UICorner")
 local shindolife = Instance.new("TextButton")
-local drag = Instance.new("TextLabel")
+local UICorner_4 = Instance.new("UICorner")
+local UIGradient_4 = Instance.new("UIGradient")
+local dahood = Instance.new("TextButton")
+local UIGradient_5 = Instance.new("UIGradient")
+local UICorner_5 = Instance.new("UICorner")
+local VGhub = Instance.new("TextButton")
+local UICorner_6 = Instance.new("UICorner")
+local UIGradient_6 = Instance.new("UIGradient")
+local open = Instance.new("TextButton")
+local UICorner_7 = Instance.new("UICorner")
+local TextLabel_3 = Instance.new("TextLabel")
+local UIGradient_7 = Instance.new("UIGradient")
+local open_2 = Instance.new("TextButton")
+local UICorner_8 = Instance.new("UICorner")
+local TextLabel_4 = Instance.new("TextLabel")
+local UIGradient_8 = Instance.new("UIGradient")
+local Frame_2 = Instance.new("Frame")
+local UIGradient_9 = Instance.new("UIGradient")
+local UICorner_9 = Instance.new("UICorner")
+local close_2 = Instance.new("TextButton")
+local UIGradient_10 = Instance.new("UIGradient")
+local TextLabel_5 = Instance.new("TextLabel")
+local UICorner_10 = Instance.new("UICorner")
+local TextLabel_6 = Instance.new("TextLabel")
+local UICorner_11 = Instance.new("UICorner")
+local UIGradient_11 = Instance.new("UIGradient")
 local counterblox = Instance.new("TextButton")
+local UICorner_12 = Instance.new("UICorner")
+local UIGradient_12 = Instance.new("UIGradient")
+local madcity = Instance.new("TextButton")
+local UIGradient_13 = Instance.new("UIGradient")
+local UICorner_13 = Instance.new("UICorner")
+local tokyorevanger = Instance.new("TextButton")
+local UICorner_14 = Instance.new("UICorner")
+local UIGradient_14 = Instance.new("UIGradient")
 
 --Properties:
 ScreenGui.Parent = game.CoreGui
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-main.Name = "main"
-main.Parent = ScreenGui
-main.Active = true
-main.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-main.Position = UDim2.new(0.31598112, 0, 0.510884702, 0)
-main.Size = UDim2.new(0, 422, 0, 249)
-main.Active = true
-main.Draggable = true
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.Position = UDim2.new(0.340052605, 0, 0.439370096, 0)
+Frame.Size = UDim2.new(0, 449, 0, 296)
+Frame.Active = true
+Frame.Draggable = true
 
-niggahood.Name = "nigga hood"
-niggahood.Parent = main
-niggahood.BackgroundColor3 = Color3.fromRGB(0, 0, 255)
-niggahood.Position = UDim2.new(-0.000451030675, 0, 0.836561382, 0)
-niggahood.Size = UDim2.new(0, 416, 0, 40)
-niggahood.Font = Enum.Font.SourceSans
-niggahood.Text = "Dahood"
-niggahood.TextColor3 = Color3.fromRGB(0, 0, 0)
-niggahood.TextScaled = true
-niggahood.TextSize = 14.000
-niggahood.TextWrapped = true
-niggahood.MouseButton1Down:connect(function()
-	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Biteyz/Smoke/main/source')))()
-end)
-
-TextLabel.Parent = main
+TextLabel.Parent = Frame
 TextLabel.Active = true
-TextLabel.BackgroundColor3 = Color3.fromRGB(85, 85, 255)
-TextLabel.Position = UDim2.new(0.00674321456, 0, -0.000938737765, 0)
-TextLabel.Size = UDim2.new(0, 415, 0, 89)
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.Position = UDim2.new(0, 0, 0.135135129, 0)
+TextLabel.Size = UDim2.new(0, 449, 0, 50)
 TextLabel.Font = Enum.Font.SourceSans
-TextLabel.Text = "nigga moment"
+TextLabel.Text = "PIKA HUB"
 TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
 TextLabel.TextScaled = true
 TextLabel.TextSize = 14.000
 TextLabel.TextWrapped = true
 
-breaknigga.Name = "breaknigga"
-breaknigga.Parent = main
-breaknigga.BackgroundColor3 = Color3.fromRGB(172, 0, 0)
-breaknigga.Position = UDim2.new(-0.00231284183, 0, 0.675152957, 0)
-breaknigga.Size = UDim2.new(0, 416, 0, 40)
-breaknigga.Font = Enum.Font.SourceSans
-breaknigga.Text = "prison break"
-breaknigga.TextColor3 = Color3.fromRGB(0, 0, 0)
-breaknigga.TextScaled = true
-breaknigga.TextSize = 14.000
-breaknigga.TextWrapped = true
-breaknigga.MouseButton1Down:connect(function()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/chriszrk/Bowser-Hub/main/BowserHubCool", true))()
-end)
+UICorner.Parent = TextLabel
 
-Arsenal.Name = "Arsenal"
-Arsenal.Parent = main
-Arsenal.BackgroundColor3 = Color3.fromRGB(255, 178, 23)
-Arsenal.Position = UDim2.new(5.68407959e-05, 0, 0.449383229, 0)
-Arsenal.Size = UDim2.new(0, 415, 0, 56)
-Arsenal.Font = Enum.Font.SourceSans
-Arsenal.Text = "Arsenal"
-Arsenal.TextColor3 = Color3.fromRGB(0, 0, 0)
-Arsenal.TextScaled = true
-Arsenal.TextSize = 14.000
-Arsenal.TextWrapped = true
-Arsenal.MouseButton1Down:connect(function()
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub'))()
-end)
+UIGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(203, 255, 142)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(255, 255, 255))}
+UIGradient.Parent = TextLabel
 
-main2.Name = "main2"
-main2.Parent = ScreenGui
-main2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-main2.Position = UDim2.new(0.0898712426, 0, 0.256692916, 0)
-main2.Size = UDim2.new(0, 142, 0, 267)
-main2.Active = true
-main2.Draggable = true
+UIGradient_2.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(255, 240, 64)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(255, 255, 255))}
+UIGradient_2.Parent = Frame
 
-madcity.Name = "madcity"
-madcity.Parent = main2
-madcity.BackgroundColor3 = Color3.fromRGB(170, 255, 255)
-madcity.Position = UDim2.new(0.0212559178, 0, 0.694976211, 0)
-madcity.Size = UDim2.new(0, 138, 0, 79)
-madcity.Font = Enum.Font.SourceSans
-madcity.Text = "madcity"
-madcity.TextColor3 = Color3.fromRGB(0, 0, 0)
-madcity.TextScaled = true
-madcity.TextSize = 14.000
-madcity.TextWrapped = true
-madcity.MouseButton1Down:connect(function()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptRUs/Banana/main/MadCityFast.lua", true))()
-end)
+UICorner_2.Parent = Frame
+
+close.Name = "close"
+close.Parent = Frame
+close.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+close.Position = UDim2.new(0.824053407, 0, -0.00047879605, 0)
+close.Size = UDim2.new(0, 79, 0, 48)
+close.Font = Enum.Font.SourceSans
+close.Text = ""
+close.TextColor3 = Color3.fromRGB(0, 0, 0)
+close.TextSize = 14.000
+
+UIGradient_3.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(255, 255, 56)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(255, 255, 255))}
+UIGradient_3.Parent = close
+
+TextLabel_2.Parent = close
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.BackgroundTransparency = 1.000
+TextLabel_2.Position = UDim2.new(-0.123928189, 0, -0.188333273, 0)
+TextLabel_2.Size = UDim2.new(0, 98, 0, 50)
+TextLabel_2.Font = Enum.Font.SourceSansBold
+TextLabel_2.Text = "close"
+TextLabel_2.TextColor3 = Color3.fromRGB(252, 252, 252)
+TextLabel_2.TextSize = 41.000
+TextLabel_2.TextStrokeTransparency = 0.270
+
+UICorner_3.Parent = close
 
 shindolife.Name = "shindo life"
-shindolife.Parent = main2
-shindolife.BackgroundColor3 = Color3.fromRGB(255, 112, 17)
-shindolife.Position = UDim2.new(0.00718853809, 0, 0.480416238, 0)
-shindolife.Size = UDim2.new(0, 138, 0, 51)
+shindolife.Parent = Frame
+shindolife.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+shindolife.Position = UDim2.new(0, 0, 0.375, 0)
+shindolife.Size = UDim2.new(0, 200, 0, 50)
 shindolife.Font = Enum.Font.SourceSans
-shindolife.Text = "shindo life nigga hub spec"
+shindolife.Text = "shindo life"
 shindolife.TextColor3 = Color3.fromRGB(0, 0, 0)
 shindolife.TextScaled = true
 shindolife.TextSize = 14.000
@@ -118,25 +121,166 @@ shindolife.MouseButton1Down:connect(function()
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub'))()
 end)
 
-drag.Name = "drag"
-drag.Parent = main2
-drag.BackgroundColor3 = Color3.fromRGB(85, 85, 255)
-drag.Position = UDim2.new(0.000303994864, 0, -0.00354821235, 0)
-drag.Size = UDim2.new(0, 137, 0, 57)
-drag.Font = Enum.Font.SourceSans
-drag.Text = "drag here"
-drag.TextColor3 = Color3.fromRGB(0, 0, 0)
-drag.TextScaled = true
-drag.TextSize = 14.000
-drag.TextWrapped = true
+UICorner_4.Parent = shindolife
+
+UIGradient_4.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(255, 255, 56)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(255, 255, 255))}
+UIGradient_4.Parent = shindolife
+
+dahood.Name = "da hood"
+dahood.Parent = Frame
+dahood.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+dahood.Position = UDim2.new(0.554565728, 0, 0.375, 0)
+dahood.Size = UDim2.new(0, 200, 0, 50)
+dahood.Font = Enum.Font.SourceSans
+dahood.Text = "da hood"
+dahood.TextColor3 = Color3.fromRGB(0, 0, 0)
+dahood.TextScaled = true
+dahood.TextSize = 14.000
+dahood.TextWrapped = true
+dahood.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Biteyz/Smoke/main/source')))()
+end)
+
+UIGradient_5.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(255, 255, 56)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(255, 255, 255))}
+UIGradient_5.Parent = dahood
+
+UICorner_5.Parent = dahood
+
+VGhub.Name = "V.G hub"
+VGhub.Parent = Frame
+VGhub.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+VGhub.Position = UDim2.new(0, 0, 0.668918908, 0)
+VGhub.Size = UDim2.new(0, 449, 0, 50)
+VGhub.Font = Enum.Font.SourceSans
+VGhub.Text = "Vega hub"
+VGhub.TextColor3 = Color3.fromRGB(0, 0, 0)
+VGhub.TextScaled = true
+VGhub.TextSize = 14.000
+VGhub.TextWrapped = true
+VGhub.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub'))()
+end)
+
+UICorner_6.Parent = VGhub
+
+UIGradient_6.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(255, 255, 56)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(255, 255, 255))}
+UIGradient_6.Parent = VGhub
+
+open.Name = "open"
+open.Parent = ScreenGui
+open.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+open.Position = UDim2.new(0, 0, 0.425196856, 0)
+open.Size = UDim2.new(0, 124, 0, 50)
+open.Font = Enum.Font.SourceSans
+open.Text = ""
+open.TextColor3 = Color3.fromRGB(0, 0, 0)
+open.TextSize = 14.000
+
+UICorner_7.Parent = open
+
+TextLabel_3.Parent = open
+TextLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.BackgroundTransparency = 1.000
+TextLabel_3.Position = UDim2.new(0.104838714, 0, -0.0149999997, 0)
+TextLabel_3.Size = UDim2.new(0, 98, 0, 50)
+TextLabel_3.Font = Enum.Font.SourceSansBold
+TextLabel_3.Text = "open"
+TextLabel_3.TextColor3 = Color3.fromRGB(252, 252, 252)
+TextLabel_3.TextSize = 41.000
+TextLabel_3.TextStrokeTransparency = 0.270
+
+UIGradient_7.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(255, 255, 56)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(255, 255, 255))}
+UIGradient_7.Parent = open
+
+open_2.Name = "open"
+open_2.Parent = ScreenGui
+open_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+open_2.Position = UDim2.new(0, 0, 0.270866156, 0)
+open_2.Size = UDim2.new(0, 124, 0, 50)
+open_2.Font = Enum.Font.SourceSans
+open_2.Text = ""
+open_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+open_2.TextSize = 14.000
+
+UICorner_8.Parent = open_2
+
+TextLabel_4.Parent = open_2
+TextLabel_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_4.BackgroundTransparency = 1.000
+TextLabel_4.Position = UDim2.new(0.104838721, 0, -0.0149999857, 0)
+TextLabel_4.Size = UDim2.new(0, 98, 0, 50)
+TextLabel_4.Font = Enum.Font.SourceSansBold
+TextLabel_4.Text = "open"
+TextLabel_4.TextColor3 = Color3.fromRGB(252, 252, 252)
+TextLabel_4.TextSize = 41.000
+TextLabel_4.TextStrokeTransparency = 0.270
+
+UIGradient_8.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(255, 255, 56)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(255, 255, 255))}
+UIGradient_8.Parent = open_2
+
+Frame_2.Parent = ScreenGui
+Frame_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame_2.Position = UDim2.new(0.339901477, 0, 0.0866141766, 0)
+Frame_2.Size = UDim2.new(0, 449, 0, 223)
+Frame_2.Visible = false
+Frame.Active = true
+Frame.Draggable = true
+
+UIGradient_9.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(255, 240, 64)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(255, 255, 255))}
+UIGradient_9.Parent = Frame_2
+
+UICorner_9.Parent = Frame_2
+
+close_2.Name = "close"
+close_2.Parent = Frame_2
+close_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+close_2.Position = UDim2.new(0.824053407, 0, -0.00047879605, 0)
+close_2.Size = UDim2.new(0, 79, 0, 48)
+close_2.Font = Enum.Font.SourceSans
+close_2.Text = ""
+close_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+close_2.TextSize = 14.000
+
+UIGradient_10.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(255, 255, 56)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(255, 255, 255))}
+UIGradient_10.Parent = close_2
+
+TextLabel_5.Parent = close_2
+TextLabel_5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_5.BackgroundTransparency = 1.000
+TextLabel_5.Position = UDim2.new(-0.123928189, 0, -0.188333273, 0)
+TextLabel_5.Size = UDim2.new(0, 98, 0, 50)
+TextLabel_5.Font = Enum.Font.SourceSansBold
+TextLabel_5.Text = "close"
+TextLabel_5.TextColor3 = Color3.fromRGB(252, 252, 252)
+TextLabel_5.TextSize = 41.000
+TextLabel_5.TextStrokeTransparency = 0.270
+
+UICorner_10.Parent = close_2
+
+TextLabel_6.Parent = Frame_2
+TextLabel_6.Active = true
+TextLabel_6.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_6.Position = UDim2.new(0, 0, 0.240662768, 0)
+TextLabel_6.Size = UDim2.new(0, 449, 0, 50)
+TextLabel_6.Font = Enum.Font.SourceSans
+TextLabel_6.Text = "PIKA HUB"
+TextLabel_6.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_6.TextScaled = true
+TextLabel_6.TextSize = 14.000
+TextLabel_6.TextWrapped = true
+
+UICorner_11.Parent = TextLabel_6
+
+UIGradient_11.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(203, 255, 142)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(255, 255, 255))}
+UIGradient_11.Parent = TextLabel_6
 
 counterblox.Name = "counter blox"
-counterblox.Parent = main2
-counterblox.BackgroundColor3 = Color3.fromRGB(30, 127, 255)
-counterblox.Position = UDim2.new(0.0352112651, 0, 0.259969324, 0)
-counterblox.Size = UDim2.new(0, 137, 0, 50)
+counterblox.Parent = Frame_2
+counterblox.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+counterblox.Position = UDim2.new(0, 0, 0.535804033, 0)
+counterblox.Size = UDim2.new(0, 200, 0, 50)
 counterblox.Font = Enum.Font.SourceSans
-counterblox.Text = "counter blox"
+counterblox.Text = " counter blox"
 counterblox.TextColor3 = Color3.fromRGB(0, 0, 0)
 counterblox.TextScaled = true
 counterblox.TextSize = 14.000
@@ -144,3 +288,91 @@ counterblox.TextWrapped = true
 counterblox.MouseButton1Down:connect(function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Pawel12d/hexagon/main/scripts/CounterBlox.lua"))()
 end)
+
+UICorner_12.Parent = counterblox
+
+UIGradient_12.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(255, 255, 56)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(255, 255, 255))}
+UIGradient_12.Parent = counterblox
+
+madcity.Name = "mad city"
+madcity.Parent = Frame_2
+madcity.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+madcity.Position = UDim2.new(0.554565728, 0, 0.535804033, 0)
+madcity.Size = UDim2.new(0, 200, 0, 50)
+madcity.Font = Enum.Font.SourceSans
+madcity.Text = "mad city"
+madcity.TextColor3 = Color3.fromRGB(0, 0, 0)
+madcity.TextScaled = true
+madcity.TextSize = 14.000
+madcity.TextWrapped = true
+madcity.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptRUs/Banana/main/MadCityFast.lua", true))()
+end)
+
+UIGradient_13.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(255, 255, 56)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(255, 255, 255))}
+UIGradient_13.Parent = madcity
+
+UICorner_13.Parent = madcity
+
+tokyorevanger.Name = "tokyo revanger"
+tokyorevanger.Parent = Frame_2
+tokyorevanger.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+tokyorevanger.Position = UDim2.new(0, 0, 0.776542246, 0)
+tokyorevanger.Size = UDim2.new(0, 449, 0, 50)
+tokyorevanger.Font = Enum.Font.SourceSans
+tokyorevanger.Text = "tokyo revanger made by me"
+tokyorevanger.TextColor3 = Color3.fromRGB(0, 0, 0)
+tokyorevanger.TextScaled = true
+tokyorevanger.TextSize = 14.000
+tokyorevanger.TextWrapped = true
+tokyorevanger.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://pastebin.com/raw/DCfwuFgR')))()
+end)
+
+UICorner_14.Parent = tokyorevanger
+
+UIGradient_14.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(255, 255, 56)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(255, 255, 255))}
+UIGradient_14.Parent = tokyorevanger
+
+-- Scripts:
+
+local function QLRQ_fake_script() -- close.LocalScript 
+	local script = Instance.new('LocalScript', close)
+
+	local frame = script.Parent.Parent
+	
+	script.Parent.MouseButton1Click:Connect(function()
+	   frame.Visible = false
+	end)
+end
+coroutine.wrap(QLRQ_fake_script)()
+local function HNCKO_fake_script() -- open.LocalScript 
+	local script = Instance.new('LocalScript', open)
+
+	local frame = script.Parent.Parent.Frame
+	
+	script.Parent.MouseButton1Click:Connect(function()
+	   frame.Visible = true
+	end)
+end
+coroutine.wrap(HNCKO_fake_script)()
+local function LBTRG_fake_script() -- open_2.LocalScript 
+	local script = Instance.new('LocalScript', open_2)
+
+	local frame = script.Parent.Parent.Frame
+	
+	script.Parent.MouseButton1Click:Connect(function()
+	   frame.Visible = true
+	end)
+end
+coroutine.wrap(LBTRG_fake_script)()
+local function TFIAYY_fake_script() -- close_2.LocalScript 
+	local script = Instance.new('LocalScript', close_2)
+
+	local frame = script.Parent.Parent
+	
+	script.Parent.MouseButton1Click:Connect(function()
+	   frame.Visible = false
+	end)
+end
+coroutine.wrap(TFIAYY_fake_script)()
